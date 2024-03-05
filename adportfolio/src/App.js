5b45fba5240be8a47350a46621b53cd0448cf90c
Praneetha29 <praneetha2829@gmail.com>
2024-03-05 13:56:53 +0530

    feat: custom cursor

--- adportfolio/src/App.js
+++ adportfolio/src/App.js
@@ -1,11 +1,41 @@
 import Services from './pages/Services';
 import Home from './pages/Home';
 import {Routes, Route} from 'react-router-dom';
+import {useState, useEffect} from 'react';
+import {motion} from 'framer-motion';
 import './App.scss';
 
 function App() {
+  const [mousePosition, setMousePosition] = useState({
+    x:0,
+    y:0
+  });
+  console.log(mousePosition);
+
+  useEffect(()=>{
+
+    const mouseMove=e=>{
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY
+      })
+
+    }
+
+    window.addEventListener("mousemove", mouseMove);
+    return () => {window.removeEventListener('mousemove', mouseMove)};
+  },[]);
+
+  const variants = {
+    default:{
+      x:mousePosition.x-16,
+      y:mousePosition.y-16
+    }
+  }
+
   return (
     <>
+    <motion.div className='cursor' variants={variants} animate="default"/>
       <Routes>
       <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
